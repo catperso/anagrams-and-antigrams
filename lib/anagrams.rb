5984724180f955ac5dd66e0dda3b7745c0dd5419
Matt Casperson <mc.casperson@gmail.com>
2021-12-10 09:26:42 -0800

    add/pass fourth spec to disregard words without vowels

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,10 +2,12 @@ class Anagramizer
   def anagram(word1, word2)
     word1_compare = word1.downcase.split('').sort
     word2_compare = word2.downcase.split('').sort
-    if word1_compare == word2_compare
+    if (word1_compare.none? {|e| e.match(/[aeiou]/i)}) || (word2_compare.none? {|e| e.match(/[aeiou]/i)})
+      "I don't think those are real words D:"
+    elsif word1_compare == word2_compare
       "Those words are anagrams!"
     else
-      "Those words are not anagrams :("
+      "Those words are not anagrams ):"
     end
   end
 
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -9,12 +9,17 @@ describe('Anagramizer#anagram') do
 
   it('will tell you if two words are not anagrams') do
     auntie_gram = Anagramizer.new
-    expect(auntie_gram.anagram("rhythm", "phlegm")).to(eq("Those words are not anagrams :("))
+    expect(auntie_gram.anagram("crying", "phlegm")).to(eq("Those words are not anagrams ):"))
   end
 
   it('will tell you if the words are anagrams regardless of case') do
     auntie_gram = Anagramizer.new
     expect(auntie_gram.anagram("Tea", "Eat")).to(eq("Those words are anagrams!"))
+  end
+
+  it('will complain if you do not input words with at least one vowel') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.anagram("Phblthp", "Phtlbhp")).to(eq("I don't think those are real words D:"))
   end
 
 end
