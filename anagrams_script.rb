b5e8e5b1f5871f1fc464a6237157e248a52923c5
Matt Casperson <mc.casperson@gmail.com>
2021-12-10 10:54:21 -0800

    add anagrams_script.rb script to run the application

--- anagrams_script.rb
+++ anagrams_script.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+
+require('./lib/anagrams.rb')
+
+puts "Welcome to the anagramizer!"
+puts "Please enter the first word/phrase to compare:"
+word1 = gets.chomp
+puts "Please enter the second word/phrase to compare:"
+word2 = gets.chomp
+auntie_gram = Anagramizer.new
+puts auntie_gram.anagram(word1, word2)
