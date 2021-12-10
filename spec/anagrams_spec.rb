776e636dfcfbcec389da2b2aa25e137222c31c2d
Matt Casperson <mc.casperson@gmail.com>
2021-12-10 08:54:53 -0800

    add/pass second spec when the words are not anagrams

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,10 +1,15 @@
 require('rspec')
 require('anagrams')
 
-describe('Anagramizer') do
+describe('Anagramizer#anagram') do
   it('will tell you if two words are anagrams') do
     auntie_gram = Anagramizer.new
     expect(auntie_gram.anagram("ruby", "bury")).to(eq("Those words are anagrams!"))
+  end
+
+  it('will tell you if two words are not anagrams') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.anagram("rhythm", "phlegm")).to(eq("Those words are not anagrams :("))
   end
 
 end
