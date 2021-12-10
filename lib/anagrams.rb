43345451a96087381f4d19b24ed6ec55dc0151f7
Matt Casperson <mc.casperson@gmail.com>
2021-12-10 08:53:01 -0800

    add first spec and code to match

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,17 @@
 class Anagramizer
+  def anagram(word1, word2)
+    word1_compare = word1.split('').sort
+    word2_compare = word2.split('').sort
+    if word1_compare == word2_compare
+      "Those words are anagrams!"
+    else
+      "Those words are not anagrams :("
+    end
+  end
 
 
 
 
 
 
-  
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,3 +1,10 @@
 require('rspec')
 require('anagrams')
 
+describe('Anagramizer') do
+  it('will tell you if two words are anagrams') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.anagram("ruby", "bury")).to(eq("Those words are anagrams!"))
+  end
+
+end
