be332774bf5a99fd0734d0cee8758e91b8d82a25
Matt Casperson <mc.casperson@gmail.com>
2021-12-10 14:28:58 -0800

    add Anagramizer.palindrome(), with specs, to check which of the inputs are palindromes

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,7 @@
 class Anagramizer
   def anagram(word1, word2)
-    word1_compare = word1.downcase.split('').sort
-    word2_compare = word2.downcase.split('').sort
-    word1_compare.delete_if{|x| x.match(/[^a-z]/i)}
-    word2_compare.delete_if{|x| x.match(/[^a-z]/i)}
+    word1_compare = word1.downcase.split('').sort.delete_if{|x| x.match(/[^a-z]/i)}
+    word2_compare = word2.downcase.split('').sort.delete_if{|x| x.match(/[^a-z]/i)}
     if (word1_compare.none? {|e| e.match(/[aeiou]/i)}) || (word2_compare.none? {|e| e.match(/[aeiou]/i)})
       "I don't think those are real words D:"
     elsif word1_compare == word2_compare
@@ -25,9 +23,25 @@ class Anagramizer
     end
   end
 
-
-
-
-
-
+  def palindrome(word1, word2)
+    word1_array = word1.downcase.split('').delete_if{|x| x.match(/[^a-z]/i)}
+    word2_array = word2.downcase.split('').delete_if{|x| x.match(/[^a-z]/i)}
+    word1_results = false
+    word2_results = false
+    if word1_array == word1_array.reverse
+      word1_results = true
+    end
+    if word2_array == word2_array.reverse
+      word2_results = true
+    end
+    if word1_results && word2_results
+      "Both words/phrases are palindromes!"
+    elsif word1_results && !word2_results
+      "The first word/phrase is a palindrome but the second is not."
+    elsif word2_results && !word1_results
+      "The second word/phrase is a palindrome but the first is not."
+    else
+      "Neither word/phrase is a palindrome :("
+    end
+  end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -32,3 +32,30 @@ describe('Anagramizer#anagram') do
     expect(auntie_gram.anagram("The Morse Code", "Here come dots!")).to(eq("Those words are anagrams!"))
   end
 end
+
+describe('Anagramizer#palindrome') do
+  it('will tell you if both words are palindromes') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.palindrome("racecar", "civic")).to(eq("Both words/phrases are palindromes!"))
+  end
+
+  it('will tell you if the only the first word is a palindrome') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.palindrome("racecar", "cats")).to(eq("The first word/phrase is a palindrome but the second is not."))
+  end
+
+  it('will tell you if the only the second word is a palindrome') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.palindrome("horrible", "civic")).to(eq("The second word/phrase is a palindrome but the first is not."))
+  end
+
+  it('will tell you if neither word is a palindrome') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.palindrome("horrible", "cats")).to(eq("Neither word/phrase is a palindrome :("))
+  end
+
+  it('will work on entire sentences, regardless of case or punctuation') do
+    auntie_gram = Anagramizer.new
+    expect(auntie_gram.palindrome("Mr. Owl ate my metal worm", "Do geese see God?")).to(eq("Both words/phrases are palindromes!"))
+  end
+end
